2801cc081049f484c4f86c57a05a748c2fde7fb1
Jack <2091813161@qq.com>
2025-02-20 15:19:39 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
-  base: '/blog/',
+  base: "/blog/",
   title: 'VuePress',
   description: 'My first VuePress Site',
 
